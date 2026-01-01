0f8ccecfe962fac05eb45f8064f27ce5dce3fbb5
justinosh06 <justinosh060721@gmail.com>
2026-01-01 10:50:20 +0800

    feat: add utility to get current geolocation

---
+++ lib/geo/geolocation.ts
@@ -0,0 +1,15 @@
+export function getCurrentLocation(): Promise<GeolocationPosition> {
+    return new Promise((resolve, reject) => {
+        if (!navigator.geolocation) {
+            reject(new Error('Geolocation is not supported by this browser.'));
+        }
+        navigator.geolocation.getCurrentPosition(
+            resolve,
+            reject,
+            {
+                enableHighAccuracy: true,
+                timeout: 1000,
+            }
+        );
+    });
+}
